09f796d483ca1cd1dc857e6cdf9a0c4237b8b7b6
ermakov nikita <ermakov.nd@phystech.edu>
2020-03-11 18:57:26 +0300

    moved state update in handlers into iteration cycles

--- src/main/java/ru/sbt/mipt/oop/DoorSensorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/DoorSensorEventHandler.java
@@ -1,7 +1,5 @@
 package ru.sbt.mipt.oop;
 
-import javafx.util.Pair;
-
 public class DoorSensorEventHandler implements SensorEventHandler {
     private final SmartHome smartHome;
 
@@ -13,13 +11,15 @@ public class DoorSensorEventHandler implements SensorEventHandler {
     public void handle(SensorEvent event) {
         if (!isDoorEvent(event)) return;
 
-        Room room = findRoom(event);
-        Door door = findDoor(event);
-        if (room == null || door == null) return;
-
-        boolean setDoorOpen = event.getType() == SensorEventType.DOOR_OPEN;
-        door.setOpen(setDoorOpen);
-        logEvent(room, door, setDoorOpen ? "opened" : "closed");
+        for (Room room : smartHome.getRooms()) {
+            for (Door door : room.getDoors()) {
+                if (door.getId().equals(event.getObjectId())) {
+                    boolean setDoorOpen = event.getType() == SensorEventType.DOOR_OPEN;
+                    door.setOpen(setDoorOpen);
+                    logEvent(room, door, setDoorOpen ? "opened" : "closed");
+                }
+            }
+        }
     }
 
     private void logEvent(Room room, Door door, String setDoor) {
@@ -28,24 +28,5 @@ public class DoorSensorEventHandler implements SensorEventHandler {
 
     private boolean isDoorEvent(SensorEvent event) {
         return event.getType() == SensorEventType.DOOR_OPEN || event.getType() == SensorEventType.DOOR_CLOSED;
-    }
-
-    private Room findRoom(SensorEvent event) {
-        return findRoomAndDoor(event).getKey();
-    }
-
-    private Door findDoor(SensorEvent event) {
-        return findRoomAndDoor(event).getValue();
-    }
-
-    private Pair<Room, Door> findRoomAndDoor(SensorEvent event) {
-        for (Room room : smartHome.getRooms()) {
-            for (Door door : room.getDoors()) {
-                if (door.getId().equals(event.getObjectId())) {
-                    return new Pair<>(room, door);
-                }
-            }
-        }
-        return new Pair<>(null, null);
     }
 }
--- src/main/java/ru/sbt/mipt/oop/LightSensorEventHandler.java
+++ src/main/java/ru/sbt/mipt/oop/LightSensorEventHandler.java
@@ -1,7 +1,5 @@
 package ru.sbt.mipt.oop;
 
-import javafx.util.Pair;
-
 public class LightSensorEventHandler implements SensorEventHandler {
     private final SmartHome smartHome;
 
@@ -13,13 +11,15 @@ public class LightSensorEventHandler implements SensorEventHandler {
     public void handle(SensorEvent event) {
         if (!isLightEvent(event)) return;
 
-        Room room = findRoom(event);
-        Light light = findLight(event);
-        if (room == null || light == null) return;
-
-        boolean setLightOn = event.getType() == SensorEventType.LIGHT_ON;
-        light.setOn(setLightOn);
-        logEvent(room, light, setLightOn ? "on" : "off");
+        for (Room room : smartHome.getRooms()) {
+            for (Light light : room.getLights()) {
+                if (light.getId().equals(event.getObjectId())) {
+                    boolean setLightOn = event.getType() == SensorEventType.LIGHT_ON;
+                    light.setOn(setLightOn);
+                    logEvent(room, light, setLightOn ? "on" : "off");
+                }
+            }
+        }
     }
 
     private void logEvent(Room room, Light light, String setLight) {
@@ -28,24 +28,5 @@ public class LightSensorEventHandler implements SensorEventHandler {
 
     private boolean isLightEvent(SensorEvent event) {
         return event.getType() == SensorEventType.LIGHT_ON || event.getType() == SensorEventType.LIGHT_OFF;
-    }
-
-    private Room findRoom(SensorEvent event) {
-        return findRoomAndLight(event).getKey();
-    }
-
-    private Light findLight(SensorEvent event) {
-        return findRoomAndLight(event).getValue();
-    }
-
-    private Pair<Room, Light> findRoomAndLight(SensorEvent event) {
-        for (Room room : smartHome.getRooms()) {
-            for (Light light : room.getLights()) {
-                if (light.getId().equals(event.getObjectId())) {
-                    return new Pair<>(room, light);
-                }
-            }
-        }
-        return new Pair<>(null, null);
     }
 }
